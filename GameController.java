7285a171c858855b797ff24d79c84dabec8a08bc
antonfyhr <166555428+antonfyhr@users.noreply.github.com>
2026-03-03 17:37:42 +0100

    Annat

--- GameController.java
+++ GameController.java
@@ -1,97 +1,76 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.ArrayList;
 
-public class GameController {
-    private final int delay = 50;
-    private Timer timer = new Timer(delay, new CarController.TimerListener());
-    ArrayList<Vehicle> cars = new ArrayList<>();
-    Garage volvoWorkshops;
-    GameLogic gl;
-    GameView gv;
+public class GameController{
 
-    public void GameController(GameLogic gl, GameView gv){
-        this.gl = gl;
-        this.gv = gv;
+    GameState gs;
 
+    public GameController(GameState gs) {
+        this.gs = gs;
     }
 
-    private class TimerListener implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
-            for (Vehicle car : cars) {
-                gl.update();
-                if(gamelogic.inGarageTest(volvoWorkshops, car)) {continue;}
-                gamelogic.turnAroundTest(car);
-                car.move(); //ska till gamelogic
-                gv.drawPanel.repaint();
-                gamelogic.loadOnGarageIfClose(volvoWorkshops, car, cars);
+    public void initComp() {
+        gs.gv.gasButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                gs.gl.ml.gas(gs.gv.gasAmount);
             }
-        }
+        });
+        gs.gv.brakeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                gs.gl.ml.brake(gs.gv.brakeAmount);
+            }
+        });
+        gs.gv.turboOnButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                gs.gl.ml.saabTurboOn();
+            }
+        });
+        gs.gv.liftBedButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                gs.gl.ml.scaniaLiftBed();
+            }
+        });
+        gs.gv.turboOffButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                gs.gl.ml.saabTurboOff();
+            }
+        });
+        gs.gv.lowerBedButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                gs.gl.ml.lowerLiftBed();
+            }
+        });
+        gs.gv.startButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                gs.gl.ml.startAllCars();
+            }
+        });
+        gs.gv.stopButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                gs.gl.ml.stopAllCars();
+            }
+        });
+        gs.gv.turnLeftButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                gs.gl.ml.turnLeft();
+            }
+        });
+        gs.gv.turnRightButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                gs.gl.ml.turnRight();
+            }
+        });
     }
-    //Actionlisteners
-        gv.gasButton.addActionListener(new ActionListener() {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            cc.gas(gasAmount);
-        }
-    });
-        brakeButton.addActionListener(new ActionListener() {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            cc.brake(brakeAmount);
-        }
-    });
-        turboOnButton.addActionListener(new ActionListener() {
-        @Override
-        public void actionPerformed(ActionEvent e) {cc.saabTurboOn();
-        }
-    });
-        liftBedButton.addActionListener(new ActionListener() {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            cc.scaniaLiftBed();
-        }
-    });
-        turboOffButton.addActionListener(new ActionListener() {
-        @Override
-        public void actionPerformed(ActionEvent e) {cc.saabTurboOff();
-        }
-    });
-        lowerBedButton.addActionListener(new ActionListener() {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            cc.lowerLiftBed();
-        }
-    });
-        startButton.addActionListener(new ActionListener() {
-        @Override
-        public void actionPerformed(ActionEvent e) {cc.startAllCars();
-        }
-    });
-        stopButton.addActionListener(new ActionListener() {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            cc.stopAllCars();
-        }
-    });
-        turnLeftButton.addActionListener(new ActionListener() {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            cc.turnLeft();
-        }
-    });
-        turnRightButton.addActionListener(new ActionListener() {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            cc.turnRight();
-        }
-    });
-
-
-
-
-
-
-
 }
